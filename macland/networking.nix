47ce002f8747cc3cd2312a3ac429a81ef1b2d7a1
Celes Renata <celes@celestium.life>
2024-04-22 18:53:43 -0700

    adding lan-mouse

--- macland/networking.nix
+++ macland/networking.nix
@@ -33,7 +33,7 @@
     ];
     networking.firewall = {
       enable = true;
-      allowedTCPPorts = [ 47984 47989 47990 48010 ];
+      allowedTCPPorts = [ 4242 47984 47989 47990 48010 ];
       allowedUDPPortRanges = [
         { from = 47998; to = 48000; }
         { from = 8000; to = 8010; }
