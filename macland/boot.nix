53d0776119ade7baae1b63a9cfa17a85f557c217
Celes Renata <celes@celestium.life>
2024-07-12 20:31:42 -0700

    add nfs and ntfs support

--- macland/boot.nix
+++ macland/boot.nix
@@ -1,18 +1,18 @@
 { ... }:
 {
   config = {
-    boot.loader = {
-      efi = {
-        efiSysMountPoint = "/boot/EFI";
+    boot = {
+      loader = {
+        efi.efiSysMountPoint = "/boot/EFI";
+        grub = {
+          efiSupport = true;
+          efiInstallAsRemovable = true;
+          device = "nodev";
+        };
       };
-
-      grub = {
-        efiSupport = true;
-        efiInstallAsRemovable = true;
-        device = "nodev";
-      };
+      supportedFilesystems = [ "ntfs" "nfs" ];
+      #kernelPackages = pkgs.t2Kernel;
+      kernelModules = [ "uinput" "amdgpu" ];
     };
-    #boot.kernelPackages = pkgs.t2Kernel;
-    boot.kernelModules = [ "uinput" "amdgpu" ];
   };
 }
