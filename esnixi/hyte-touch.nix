9fc6d455d3f0e1c5431868ed5f728d7e6f4a0200
Celes Renata <celes@frameshift.net>
2025-10-11 02:53:19 -0700

    Use quickshell directly for touch interface

--- esnixi/hyte-touch.nix
+++ esnixi/hyte-touch.nix
@@ -22,5 +22,9 @@
   environment.systemPackages = with pkgs; [
     weston
     gamescope
+    # Add the hyte-touch-widgets from our flake
+    (callPackage /home/celes/sources/celesrenata/hyte-touch-infinite-flakes/packages/touch-widgets.nix {
+      inherit (inputs) quickshell;
+    })
   ];
 }
