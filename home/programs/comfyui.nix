460ab2b3b6c2a02532d6f00811b9c9cd809fd573
Celes Renata <celes@frameshift.net>
2026-07-10 00:51:07 -0700

    ComfyUI: add kernels package for FP8 inference

--- home/programs/comfyui.nix
+++ home/programs/comfyui.nix
@@ -36,7 +36,7 @@ let
         -r ${comfyHome}/app/requirements.txt \
         pip pyyaml pycryptodome pyOpenSSL segment-anything dill facexlib \
         piexif insightface deepdiff webcolors ultralytics py-cpuinfo gguf \
-        llama-cpp-python onnxruntime imageio-ffmpeg opencv-python numba pynvml timm natsort
+        llama-cpp-python onnxruntime imageio-ffmpeg opencv-python numba pynvml timm natsort kernels
       rm -f ${comfyHome}/.deps-*
       touch ${comfyHome}/.deps-${comfyui.version}
     fi
